d0c9b6e53778105c233ab84db5d040059c6011f1
Cynric_Ni <cynric_ni@163.com>
2021-03-05 00:11:00 +0800

    2021/3/5

---
+++ Graphic2/Graphic2/Graphic2View.cpp
@@ -0,0 +1,176 @@
+﻿
+// Graphic2View.cpp: CGraphic2View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Graphic2.h"
+#endif
+
+#include "Graphic2Doc.h"
+#include "Graphic2View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CGraphic2View
+
+IMPLEMENT_DYNCREATE(CGraphic2View, CView)
+
+BEGIN_MESSAGE_MAP(CGraphic2View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_COMMAND(IDM_DOT, &CGraphic2View::OnDot)
+	ON_COMMAND(IDM_LINE, &CGraphic2View::OnLine)
+	ON_COMMAND(IDM_RECTANGLE, &CGraphic2View::OnRectangle)
+	ON_COMMAND(IDM_ELLIPSE, &CGraphic2View::OnEllipse)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+// CGraphic2View 构造/析构
+
+CGraphic2View::CGraphic2View() noexcept
+{
+	// TODO: 在此处添加构造代码
+	m_nDrawType = 0;
+
+}
+
+CGraphic2View::~CGraphic2View()
+{
+}
+
+BOOL CGraphic2View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CGraphic2View 绘图
+
+void CGraphic2View::OnDraw(CDC* pDC)
+{
+	CGraphic2Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CGraphic2View 打印
+
+BOOL CGraphic2View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CGraphic2View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CGraphic2View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CGraphic2View 诊断
+
+#ifdef _DEBUG
+void CGraphic2View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CGraphic2View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CGraphic2Doc* CGraphic2View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CGraphic2Doc)));
+	return (CGraphic2Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CGraphic2View 消息处理程序
+
+
+void CGraphic2View::OnDot()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nDrawType = 1;
+}
+
+
+void CGraphic2View::OnLine()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nDrawType = 2;
+}
+
+
+void CGraphic2View::OnRectangle()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nDrawType = 3;
+}
+
+
+void CGraphic2View::OnEllipse()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nDrawType = 4;
+}
+
+
+void CGraphic2View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	m_ptOrigin = point;
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CGraphic2View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CClientDC dc(this);
+	CPen pen(PS_SOLID, 1, RGB(255, 0, 0, 0));
+	dc.SelectObject(&pen);
+	CBrush* pBrush = CBrush::FromHandle((HBRUSH)GetStockObject(NULL_BRUSH));
+	switch (m_nDrawType)
+	{
+	case 1:
+		dc.SetPixel(point, RGB(255, 0, 0));
+		break;
+	case 2:
+		dc.MoveTo(m_ptOrigin);
+		dc.LineTo(point);
+		break;
+	case 3:
+		dc.Rectangle(CRect(m_ptOrigin, point));
+		break;
+	case 4:
+		dc.Ellipse(CRect(m_ptOrigin, point));
+		break;
+	}
+	CView::OnLButtonUp(nFlags, point);
+}
+
---
+++ Graphic2/Graphic2/Graphic2View.h
@@ -0,0 +1,59 @@
+﻿
+// Graphic2View.h: CGraphic2View 类的接口
+//
+
+#pragma once
+
+
+class CGraphic2View : public CView
+{
+protected: // 仅从序列化创建
+	CGraphic2View() noexcept;
+	DECLARE_DYNCREATE(CGraphic2View)
+
+// 特性
+public:
+	CGraphic2Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CGraphic2View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+private:
+	UINT		m_nDrawType;
+	CPoint		m_ptOrigin;
+public:
+	afx_msg void OnDot();
+	afx_msg void OnLine();
+	afx_msg void OnRectangle();
+	afx_msg void OnEllipse();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // Graphic2View.cpp 中的调试版本
+inline CGraphic2Doc* CGraphic2View::GetDocument() const
+   { return reinterpret_cast<CGraphic2Doc*>(m_pDocument); }
+#endif
+
