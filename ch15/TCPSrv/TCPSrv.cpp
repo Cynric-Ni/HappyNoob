936fa25e7ad1584bf9cf74781306cdb027c88da3
cynric_ni <cynric_ni@hotmail.com>
2021-03-29 15:24:28 +0800

    ch15

---
+++ ch15/TCPSrv/TCPSrv.cpp
@@ -0,0 +1,74 @@
+﻿// TCPSrv.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+//#include "pch.h"
+
+int main()
+{
+  //加载套接字库
+	WORD   wVersionRequested;
+	WSADATA  wsaData;
+	int err;
+
+	wVersionRequested = MAKEWORD(1, 1);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		return -1;
+	}
+
+	if (LOBYTE(wsaData.wVersion) != 1 ||
+		HIBYTE(wsaData.wVersion) != 1) {
+		WSACleanup();
+		return -1;
+	}
+  //创建于监听的套接字
+	SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons(6000);
+
+  //绑定套接字
+	bind(sockSrv, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
+ //将套接字设为监听模式，准备接收客户请求
+	listen(sockSrv, 5);
+
+	SOCKADDR_IN  addrClient;
+	int len = sizeof(SOCKADDR);
+
+	while (1)
+	{
+		//等待客户请求到来
+		SOCKET sockConn = accept(sockSrv, (SOCKADDR*)&addrClient, &len);
+		char sendBuf[100];
+		char str[INET_ADDRSTRLEN];
+		sprintf_s(sendBuf, 100, "Welcome %s to http://www.phei.com.cn",
+			inet_ntop(AF_INET, &addrClient.sin_addr, str, sizeof(str)));
+		//发送数据
+		send(sockConn, sendBuf, strlen(sendBuf) + 1, 0);
+		char recvBuf[100];
+		//接受数据
+		recv(sockConn, recvBuf, 100, 0);
+		//打印接收的数据
+		printf("%s\n", recvBuf);
+		//关闭套接字
+		closesocket(sockConn);
+	}
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
