61371b8e08fe399284d2ff765277bb7ec1e39baf
cynric_ni <cynric_ni@hotmail.com>
2021-04-12 16:23:52 +0800

    4/12

---
+++ ch17/chat/chatDlg.h
@@ -0,0 +1,38 @@
+﻿
+// chatDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CchatDlg 对话框
+class CchatDlg : public CDialogEx
+{
+// 构造
+public:
+	CchatDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CHAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg LRESULT onSock(WPARAM, LPARAM);
+	DECLARE_MESSAGE_MAP()
+private:
+	SOCKET  m_socket;
+	BOOL	InitSocket();
+};
