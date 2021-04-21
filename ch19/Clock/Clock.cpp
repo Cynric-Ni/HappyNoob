912d1ad2caf12c0a7155c6a587979f9d5b673ab5
Cynric_Ni <cynric_ni@163.com>
2021-04-21 23:22:18 +0800

    2021/4/21 开始ch19

---
+++ ch19/Clock/Clock.cpp
@@ -0,0 +1,77 @@
+﻿// Clock.cpp: CClockApp 和 DLL 注册的实现。
+
+#include "pch.h"
+#include "framework.h"
+#include "Clock.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+CClockApp theApp;
+
+const GUID CDECL _tlid = {0xab4b8360,0xe516,0x45e2,{0x82,0xcf,0xec,0x76,0xdc,0xb9,0x6c,0xbe}};
+const WORD _wVerMajor = 1;
+const WORD _wVerMinor = 0;
+
+
+
+// CClockApp::InitInstance - DLL 初始化
+
+BOOL CClockApp::InitInstance()
+{
+	BOOL bInit = COleControlModule::InitInstance();
+
+	if (bInit)
+	{
+		// TODO:  在此添加您自己的模块初始化代码。
+	}
+
+	return bInit;
+}
+
+
+
+// CClockApp::ExitInstance - DLL 终止
+
+int CClockApp::ExitInstance()
+{
+	// TODO:  在此添加您自己的模块终止代码。
+
+	return COleControlModule::ExitInstance();
+}
+
+
+
+// DllRegisterServer - 将项添加到系统注册表
+
+STDAPI DllRegisterServer(void)
+{
+	AFX_MANAGE_STATE(_afxModuleAddrThis);
+
+	if (!AfxOleRegisterTypeLib(AfxGetInstanceHandle(), _tlid))
+		return ResultFromScode(SELFREG_E_TYPELIB);
+
+	if (!COleObjectFactoryEx::UpdateRegistryAll(TRUE))
+		return ResultFromScode(SELFREG_E_CLASS);
+
+	return NOERROR;
+}
+
+
+
+// DllUnregisterServer - 将项从系统注册表中移除
+
+STDAPI DllUnregisterServer(void)
+{
+	AFX_MANAGE_STATE(_afxModuleAddrThis);
+
+	if (!AfxOleUnregisterTypeLib(_tlid, _wVerMajor, _wVerMinor))
+		return ResultFromScode(SELFREG_E_TYPELIB);
+
+	if (!COleObjectFactoryEx::UpdateRegistryAll(FALSE))
+		return ResultFromScode(SELFREG_E_CLASS);
+
+	return NOERROR;
+}
---
+++ ch19/Clock/ClockPropPage.cpp
@@ -0,0 +1,50 @@
+﻿// ClockPropPage.cpp : CClockPropPage 属性页类的实现。
+
+#include "pch.h"
+#include "framework.h"
+#include "Clock.h"
+#include "ClockPropPage.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+IMPLEMENT_DYNCREATE(CClockPropPage, COlePropertyPage)
+
+// 消息映射
+
+BEGIN_MESSAGE_MAP(CClockPropPage, COlePropertyPage)
+END_MESSAGE_MAP()
+
+// 初始化类工厂和 guid
+
+IMPLEMENT_OLECREATE_EX(CClockPropPage, "MFCACTIVEXCONT.ClockPropPage.1",
+	0x80805d3d,0x834b,0x45de,0x83,0xd3,0x22,0xb6,0x61,0xc1,0x30,0x5a)
+
+// CClockPropPage::CClockPropPageFactory::UpdateRegistry -
+// 添加或移除 CClockPropPage 的系统注册表项
+
+BOOL CClockPropPage::CClockPropPageFactory::UpdateRegistry(BOOL bRegister)
+{
+	if (bRegister)
+		return AfxOleRegisterPropertyPageClass(AfxGetInstanceHandle(),
+			m_clsid, IDS_CLOCK_PPG);
+	else
+		return AfxOleUnregisterClass(m_clsid, nullptr);
+}
+
+// CClockPropPage::CClockPropPage - 构造函数
+
+CClockPropPage::CClockPropPage() :
+	COlePropertyPage(IDD, IDS_CLOCK_PPG_CAPTION)
+{
+}
+
+// CClockPropPage::DoDataExchange - 在页和属性间移动数据
+
+void CClockPropPage::DoDataExchange(CDataExchange* pDX)
+{
+	DDP_PostProcessing(pDX);
+}
+
+// CClockPropPage 消息处理程序
