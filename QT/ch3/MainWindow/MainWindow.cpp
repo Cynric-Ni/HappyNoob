d8d2d6e03f25cb06fce4c99ae75987786f23a523
Cynric_Ni <cynric_ni@163.com>
2021-06-10 01:23:03 +0800

    2021/6/10

--- QT/ch3/MainWindow/MainWindow.cpp
+++ QT/ch3/MainWindow/MainWindow.cpp
@@ -355,3 +355,13 @@ void MainWindow::find()
     findDialog->activateWindow();
 
 }
+
+void MainWindow::goToCell()
+{
+    GoToCellDialog dialog(this);
+    if (dialog.exec()) {
+        QString str = dialog.lineEdit->text().toUpper();
+        spreadsheet->setCurrentCell(str.mid(1).toInt() - 1,
+            str[0].unicode() - 'A');
+    }
+}
