585e2125645c96c2e98c37b67229ad9c5782c3ef
cynric_ni <cynric_ni@hotmail.com>
2021-08-02 21:16:25 +0800

    8-2 work

---
+++ QT/ch5/HEXSPINBOX/HEXSPINBOX/hexspinbox.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QSpinBox>
+
+class QRegExpValidator;
+
+class HexSpinBox :public QSpinBox
+{
+	Q_OBJECT
+public:
+	HexSpinBox(QWidget* parent = 0);
+protected:
+	QValidator::State validate(QString& text, int& pos) const;
+	int valueFromText(const QString& text) const;
+	QString textFromValue(int value) const;
+
+private:
+	QRegularExpressionValidator* validator;
+};
