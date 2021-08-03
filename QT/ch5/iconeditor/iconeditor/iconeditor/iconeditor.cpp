68790d07f58220ed04168450a7acd5baa3d1041e
cynric_ni <cynric_ni@hotmail.com>
2021-08-03 16:58:57 +0800

    8-3 work

--- QT/ch5/iconeditor/iconeditor/iconeditor/iconeditor.cpp
+++ QT/ch5/iconeditor/iconeditor/iconeditor/iconeditor.cpp
@@ -1,6 +1,45 @@
+#include <QtGui>
+#include <QPalette>
 #include "iconeditor.h"
 
-iconeditor::iconeditor(QWidget *parent)
+IconEditor::IconEditor(QWidget *parent)
     : QWidget(parent)
 {
+    setAttribute(Qt::WA_StaticContents);
+    setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+    curColor = Qt::black;
+    zoom = 8;
+    image = QImage(16, 16, QImage::Format_ARGB32);
+    image.fill(qRgba(0, 0, 0, 0));
+}
+
+QSize IconEditor::sizeHint()const
+{
+    QSize size = zoom * image.size();
+    if (zoom >= 3)
+        size += QSize(1, 1);
+    return size;
+}
+
+void IconEditor::setPenColor(const QColor& newColor)
+{
+    curColor = newColor;
+}
+
+void IconEditor::setIconImage(const QImage& newImage)
+{
+    if (newImage != image) {
+        image = newImage.convertToFormat(QImage::Format_ARGB32);
+        update();
+        updateGeometry();
+    }
+}
+
+void IconEditor::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+    if (zoom >= 3) {
+        painter.setPen(*palette().WindowText());//这里书上有错误foreground被弃用，使用windowText()代替
+    }
 }
--- QT/ch5/iconeditor/iconeditor/iconeditor/iconeditor.h
+++ QT/ch5/iconeditor/iconeditor/iconeditor/iconeditor.h
@@ -4,7 +4,7 @@
 #include <QColor>
 #include <QImage>
 
-class iconeditor : public QWidget
+class IconEditor : public QWidget
 {
     Q_OBJECT
     Q_PROPERTY(QColor penColor READ penColor WRITE setPenColor)
@@ -12,14 +12,22 @@ class iconeditor : public QWidget
     Q_PROPERTY(int zoomFactor READ zoomFactor WRITE setZoomFactor)
 
 public:
-    iconeditor(QWidget *parent = Q_NULLPTR);
+    IconEditor(QWidget *parent = Q_NULLPTR);
 
     void setPenColor(const QColor& newColor);
     QColor penColor() const { return curColor; }
+    void setZoomFactor(int newZoom);
+    int zoomFactor() const { return zoom; }
+    void setIconImage(const QImage& newImage);
+    QImage iconImage() const { return image; }
+    QSize sizeHint() const;
+
+protected:
+    void mousePressEvent(QMouseEvent* event);
+    void mouseMoveEvent(QMouseEvent* event);
+    void paintEvent(QPaintEvent* event);
 
 private:
-
-
     QColor curColor;
     QImage image;
     int zoom;
