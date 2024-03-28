d85414128c01b568e88fce3092988fc616cd1a1c
cynric_ni <cynric_ni@163.com>
2024-03-28 17:33:00 +0800

    2024年3月28日单位

--- Demo/app.py
+++ Demo/app.py
@@ -1,29 +1,67 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask,render_template,request
 import webbrowser
 import os
-from threading import Timer
+from threading import Timer,Thread,Event
+import webview
 
 app = Flask(__name__)
 
-def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000/")
+app_title = "数字航道质量分析"
+host = "http://127.0.0.1"
+port = 5000
 
-    """ try:
+stop_event = Event()
+
+def run():
+    while not stop_event.is_set():
+        app.run(port=port)
+
+
+
+""" def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000/") """
+""" try:
         browser = os.environ.get('BROWSER')
         webbrowser.get(browser).open(url)
     except Exception as e:
         print(f"oh~Fuck,somewrong with browser:{e}") """
 
 @app.route("/")
-def hello():
-    chdata = [50, 20, 30, 40, 50, 60, 70, 80]
-    return render_template("index.html",chdata=chdata)
+def index():
+    f_excellent_data = [5,2,3,3,3,0,3,3]
+    f_good_data = [0,0,0,0,0,0,0,0]
+    f_average_data = [0,0,0,0,0,0,0,0]
+    f_poor_data = [0,0,0,0,0,0,0,0]
+
+    i_excellent_data = []
+    i_good_data = []
+    
+
+    return render_template("index.html",
+                           f_excellent_data = f_excellent_data,
+                           f_good_data = f_good_data,
+                           f_average_data = f_average_data,
+                           f_poor_data = f_poor_data,
+                           i_excellent_data = i_excellent_data,
+                           i_good_data = i_good_data,
+                           )
+
 if __name__ == '__main__':
-    local_url = f"http://127.0.0.1:5000/"
-    Timer(1,open_browser).start()
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+
+    webview.create_window(
+        app_title,
+        f"{host}:{port}",
+        resizable=False,
+        height=1000,
+        width=1980
+    )
+
+    webview.start()
+    """ Timer(1,open_browser).start() """
 
     """ threading.Thread(target=open_browser,args=(local_url,)).start() """
    
-    app.run()
+    
